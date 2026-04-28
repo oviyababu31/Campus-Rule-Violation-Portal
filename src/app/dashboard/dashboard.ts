fc792f90350a8219bd024fd27958f4124928028c
oviyababu31 <oviyababu67@gmail.com>
2026-04-28 22:26:35 +0530

    Updated dashboard with student count and table

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -8,15 +8,28 @@ import { FineService } from '../fine.service';
   imports: [CommonModule],
   templateUrl: './dashboard.html'
 })
-export class DashboardComponent implements OnInit { // Must be named DashboardComponent
+export class DashboardComponent implements OnInit {
+
+  students: any[] = [];
   totalFines: number = 0;
   totalAmount: number = 0;
+  totalStudents: number = 0; // ✅ NEW
 
   constructor(private fineService: FineService) {}
 
   ngOnInit() {
     const fines = this.fineService.getFines();
+
+    this.students = fines;
+
+    // Total violations
     this.totalFines = fines.length;
+
+    // Total amount
     this.totalAmount = this.fineService.getTotalFineAmount();
+
+    // Unique student count (based on rollNo)
+    const unique = new Set(fines.map(f => f.rollNo));
+    this.totalStudents = unique.size;
   }
 }
